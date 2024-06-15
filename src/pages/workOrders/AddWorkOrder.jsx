62f1992bf15a652500e1829d8d8784849a484241
hamzakamran <hamzakwebdev@gmail.com>
2024-06-15 10:00:11 -0500

    Add Request page

--- src/pages/workOrders/AddWorkOrder.jsx
+++ src/pages/workOrders/AddWorkOrder.jsx
@@ -1,4 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getUser } from "../../features/app/authSlice";
+import { useEffect } from "react";
+import { MdArrowBack } from "react-icons/md";
+import { setContent } from "../../features/app/globalSlice";
+
 const AddWorkOrder = () => {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const { accessToken } = useSelector(getUser);
+
+	useEffect(() => {
+		dispatch(
+			setContent(
+				<div className="row align-center gap-05">
+					<MdArrowBack
+						className="back-arrow"
+						onClick={() => navigate(-1)}
+					/>
+					<h3>Add Maintenance Request</h3>
+				</div>
+			)
+		);
+	});
+
 	return <div>Add Work Order</div>;
 };
 
