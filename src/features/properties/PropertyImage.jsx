c0f4b5b10a32dd1345a83ff818cb411346146514
hamzakamran <hamzakwebdev@gmail.com>
2024-06-22 13:40:53 -0500

    Images

--- src/features/properties/PropertyImage.jsx
+++ src/features/properties/PropertyImage.jsx
@@ -1,30 +1,11 @@
-import { useSelector } from "react-redux";
-import getAssetUrl from "../api/assets/getAssetUrl";
-import { getUser } from "../app/authSlice";
-import { useEffect, useState } from "react";
 import { StyledPropertyImage } from "./Properties.styled";
 import { MdDelete } from "react-icons/md";
+import { URL } from "../../constants";
 
 const PropertyImage = ({ fileKey, deleteImage }) => {
-	const { accessToken } = useSelector(getUser);
-	const [url, setUrl] = useState("");
-
-	const getUrl = async () => {
-		try {
-			const { data } = await getAssetUrl(accessToken, fileKey);
-			setUrl(data);
-		} catch (err) {
-			console.log(err.message);
-		}
-	};
-
-	useEffect(() => {
-		getUrl();
-	}, []);
-
 	return (
 		<StyledPropertyImage>
-			{url ? <img src={url} alt="Property" /> : <p>Loading image...</p>}
+			<img src={`${URL}/assets/url/${fileKey}`} alt="Property" />
 			<MdDelete onClick={() => deleteImage(fileKey)} />
 		</StyledPropertyImage>
 	);
