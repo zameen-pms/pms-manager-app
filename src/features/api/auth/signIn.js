45e662ab6cdc3fe4bbb51aed247ea69f12f76a71
hamzakamran <hamzakwebdev@gmail.com>
2024-07-16 11:03:07 -0500

    Auth

--- src/features/api/auth/signIn.js
+++ src/features/api/auth/signIn.js
@@ -5,6 +5,7 @@ const handleLogin = async (email, password) => {
 		const response = await axios.post("/auth/login", { email, password });
 		return response;
 	} catch (err) {
+		alert(err.response.data);
 		throw new Error(err.message);
 	}
 };
